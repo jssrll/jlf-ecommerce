305e128e2eb3f0c44f00f48cc5debe2fc7404787
jssrll <jessrell1010@gmail.com>
2026-04-03 16:19:18 +0800

    Update

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ let readAnnouncements = [];
 const ADMIN_PASSWORD = "jssrll101007";
 
 // Your Google Sheets Web App URL
-const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbwN1o57PLgmTrCRUjthAgbz0EZgtE1APWTUSBxNhFNRdmohkFWvroA55JFU58fmbCEE/exec";
+const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycby7otOuhGufO-5mOlJChtAqc6Tm6gReFG085NOyPvzSi8ahx_2cuFnhGx9-8cZNtPkj/exec";
 
 // PWA Install Variables
 let deferredPrompt = null;
